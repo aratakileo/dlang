f739cdfb411a0c7dee13f0c76853a5336949fbdf
TeaCondemns <tea.condemns@gmail.com>
2023-05-19 01:59:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ PACKAGE_NAME = ROOT_DIR = 'dlang'
 setup(
     name=PACKAGE_NAME,
     version='0.0.1',
-    packages=[ROOT_DIR],
+    packages=[ROOT_DIR, f'{ROOT_DIR}.resource'],
+    package_data={ROOT_DIR: ['resource/*/*']},
     url=f'https://github.com/teacondemns/{PACKAGE_NAME}',
     license='MIT',
     author='Tea Condemns',
