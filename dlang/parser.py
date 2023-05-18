8278ed8c266d6af3202ae7b23aee0e76c8ddb2d7
TeaCondemns <tea.condemns@gmail.com>
2023-05-19 00:04:20 +0300

    Update parser.py

--- dlang/parser.py
+++ dlang/parser.py
@@ -1,62 +1,82 @@
+# ATTENTION: dict key do not support newline char unlike dict value, which is also stripped of the spaces
+# and newlines chars on the right and left
+
+
 def parse(text: str):
-    parsed_data = {}
+    parsed_dict = {}
 
     special_chars_buffer = ''
-    chars_buffer = ''
-    key = ''
+    dict_value = ''
+    dict_key = ''
 
-    ignore = False
+    is_waiting_for_newline_char_to_continue = False
 
-    for char in text:
-        if ignore:
-            if char != '\n':
+    for current_char in text:
+        if is_waiting_for_newline_char_to_continue:
+            if current_char != '\n':
                 continue
 
-            ignore = False
+            dict_value += '\n'
+            is_waiting_for_newline_char_to_continue = False
+            continue
 
-        if char in '\\:=#n':
-            special_chars_buffer += char
+        if current_char in '\\:=#n':  # special chars: \, :, =, #, n
+            special_chars_buffer += current_char
 
             if special_chars_buffer == ':=':
-                has_new_line = '\n' in chars_buffer
+                has_newline = '\n' in dict_value
 
-                if not key or has_new_line:
-                    if has_new_line:
-                        lines = chars_buffer.split('\n')
+                if not dict_key or has_newline:
+                    if has_newline:
+                        # if there is newline char before `:=`
+                        lines_buffer = dict_value.split('\n')
 
-                        if key:
-                            parsed_data[key] = '\n'.join(lines[:-1]).strip().strip('\n')
+                        if dict_key:
+                            # if `dict_key` already defined then its value is set in the form of all buffered lines,
+                            # except for the last one, where `:=` is located
+                            parsed_dict[dict_key] = '\n'.join(lines_buffer[:-1]).strip().strip('\n')
 
-                        key = lines[-1].strip()
+                        dict_key = lines_buffer[-1].strip()
                     else:
-                        key = chars_buffer.strip()
+                        # if `dict_key` is not defined
+                        dict_key = dict_value.strip()
 
-                    chars_buffer = ''
+                    dict_value = ''
                 else:
-                    chars_buffer += special_chars_buffer
+                    # if `dict_key` is already defined or `:=` used in the same line as value key
+                    dict_value += special_chars_buffer
 
                 special_chars_buffer = ''
             elif special_chars_buffer == '#':
-                ignore = True
+                # this code is responsible for starting ignoring all characters after the `#` character,
+                # before the newline character or the end of the file
+
+                is_waiting_for_newline_char_to_continue = True
                 special_chars_buffer = ''
             elif len(special_chars_buffer) == 2:
+                # this next two conditions are responsible for escaping characters: \n, \\, \:, \=, \#
                 if special_chars_buffer in '\\\\\\:\\=\\#':
-                    chars_buffer += special_chars_buffer[-1]
+                    dict_value += special_chars_buffer[-1]
                 elif special_chars_buffer == '\\n':
-                    chars_buffer += '\n'
+                    dict_value += '\n'
                 else:
-                    chars_buffer += special_chars_buffer
+                    # incorrect escape combinations are simply added like other text
+                    dict_value += special_chars_buffer
 
                 special_chars_buffer = ''
 
             continue
 
         if special_chars_buffer:
-            chars_buffer += special_chars_buffer
+            # this is necessary if there are chars left in the `special_chars_buffer`
+            # and the `current_char` does not belong to special chars
+
+            dict_value += special_chars_buffer
             special_chars_buffer = ''
 
-        chars_buffer += char
+        dict_value += current_char
 
-    parsed_data[key] = chars_buffer.strip().strip('\n')
+    # set the last value to the dict
+    parsed_dict[dict_key] = dict_value.strip().strip('\n')
 
-    return parsed_data
+    return parsed_dict
