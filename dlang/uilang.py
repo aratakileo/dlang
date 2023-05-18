9dadee75810420dd0833f220d3bbf27fc60a5267
TeaCondemns <tea.condemns@gmail.com>
2023-05-19 00:25:58 +0300

    Update uilang.py

--- dlang/uilang.py
+++ dlang/uilang.py
@@ -6,11 +6,11 @@ if PLATFORM == 'windows':
     import ctypes
     import locale
 
-    def get_default_ui_language() -> str:
+    def get_sys_lang_code() -> str:
         return locale.windows_locale[ctypes.windll.kernel32.GetUserDefaultUILanguage()]
 else:
-    def get_default_ui_language():
+    def get_sys_lang_code():
         return 'en_US'
 
 
-__all__ = 'get_default_ui_language',
+__all__ = 'get_sys_lang_code',
